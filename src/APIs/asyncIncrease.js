2d7f79bbe7b3410499a1e9723760bf77957f82a4
Jiwon Jung <jw.jeoung@albamapp.com>
2017-11-08 15:32:32 +0900

    Jiwon:: add api reject condition

--- src/APIs/asyncIncrease.js
+++ src/APIs/asyncIncrease.js
@@ -1,15 +1,18 @@
 export const increaseCount1 = (value) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    if (!value) reject('invalid param')
     setTimeout(() => resolve(value), 1000)
   })
 }
 export const increaseCount2 = (value) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    if (!value) reject('invalid param')
     setTimeout(() => resolve(value), 1000)
   })
 }
 export const increaseCount3 = (value) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    if (!value) reject('invalid param')
     setTimeout(() => resolve(value), 1000)
   })
 }
